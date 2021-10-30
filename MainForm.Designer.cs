e6eee9273144b635356d08008a8044bef5a43045
Quintanamo <qherb@ycp.edu>
2021-10-30 09:26:14 -0400

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FileRenamer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilePaths = new System.Windows.Forms.TextBox();
+            this.btnBrowseFiles = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewName = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtErrorMessages = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Paths";
+            // 
+            // txtFilePaths
+            // 
+            this.txtFilePaths.Location = new System.Drawing.Point(16, 30);
+            this.txtFilePaths.Name = "txtFilePaths";
+            this.txtFilePaths.Size = new System.Drawing.Size(275, 20);
+            this.txtFilePaths.TabIndex = 1;
+            this.txtFilePaths.TextChanged += new System.EventHandler(this.txtFilePaths_TextChanged);
+            // 
+            // btnBrowseFiles
+            // 
+            this.btnBrowseFiles.Location = new System.Drawing.Point(297, 28);
+            this.btnBrowseFiles.Name = "btnBrowseFiles";
+            this.btnBrowseFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFiles.TabIndex = 2;
+            this.btnBrowseFiles.Text = "Browse";
+            this.btnBrowseFiles.UseVisualStyleBackColor = true;
+            this.btnBrowseFiles.Click += new System.EventHandler(this.btnBrowseFiles_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "New Name";
+            // 
+            // txtNewName
+            // 
+            this.txtNewName.Enabled = false;
+            this.txtNewName.Location = new System.Drawing.Point(16, 84);
+            this.txtNewName.Name = "txtNewName";
+            this.txtNewName.Size = new System.Drawing.Size(275, 20);
+            this.txtNewName.TabIndex = 4;
+            this.txtNewName.TextChanged += new System.EventHandler(this.txtNewName_TextChanged);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Enabled = false;
+            this.btnUpdate.Location = new System.Drawing.Point(297, 82);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtErrorMessages
+            // 
+            this.txtErrorMessages.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtErrorMessages.Enabled = false;
+            this.txtErrorMessages.Location = new System.Drawing.Point(16, 111);
+            this.txtErrorMessages.Multiline = true;
+            this.txtErrorMessages.Name = "txtErrorMessages";
+            this.txtErrorMessages.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtErrorMessages.Size = new System.Drawing.Size(356, 138);
+            this.txtErrorMessages.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.txtErrorMessages);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtNewName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseFiles);
+            this.Controls.Add(this.txtFilePaths);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "File Renamer for Windows";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFilePaths;
+        private System.Windows.Forms.Button btnBrowseFiles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewName;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtErrorMessages;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FileRenamer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFiles_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.InitialDirectory = "c:\\";
+                dialog.Multiselect = true;
+                dialog.Title = "Files to rename";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string[] fileNames = dialog.FileNames;
+                    if (fileNames.Count() > 0)
+                    {
+                        for (int i = 0; i < fileNames.Count(); i++)
+                        {
+                            if (i == 0 && txtFilePaths.Text.Length > 0)
+                            {
+                                txtFilePaths.Text += ",";
+                            }
+                            txtFilePaths.Text += fileNames[i];
+                            if (i != fileNames.Count() - 1)
+                            {
+                                txtFilePaths.Text += ",";
+                            }
+                        }
+                        txtNewName.Enabled = true;
+                    }
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ToggleLockFieldsAndButtons(true);
+            if (txtFilePaths.Text.Length > 0)
+            {
+                if (txtFilePaths.Text.Contains(","))
+                {
+                    string[] filePaths = txtFilePaths.Text.Split(',');
+                    Dictionary<string, List<string>> pathDictionary = new Dictionary<string, List<string>>();
+                    foreach (string path in filePaths)
+                    {
+                        string directory = Path.GetDirectoryName(path) + "\\";
+                        if (pathDictionary.ContainsKey(directory))
+                        {
+                            List<string> fileNames = pathDictionary[directory];
+                            string fileName = Path.GetFileName(path);
+                            fileNames.Add(fileName);
+                        }
+                        else
+                        {
+                            List<string> fileNames = new List<string>();
+                            string fileName = Path.GetFileName(path);
+                            fileNames.Add(fileName);
+                            pathDictionary.Add(directory, fileNames);
+                        }
+                    }
+                    bool canContinue = true;
+                    foreach (KeyValuePair<string, List<string>> pair in pathDictionary)
+                    {
+                        if (pair.Value.Count() > 1)
+                        {
+                            DialogResult result = MessageBox.Show("There are multiple files in a selected directory.  Continue renaming them with a number appended to the end?\r\n(ex. filename-1.txt)", "Multiple files in directory", MessageBoxButtons.YesNo);
+                            if (result == DialogResult.No || result == DialogResult.Cancel)
+                            {
+                                canContinue = false;
+                            }
+                        }
+                    }
+                    if (canContinue)
+                    {
+                        foreach (KeyValuePair<string, List<string>> pair in pathDictionary)
+                        {
+                            string newFileName = txtNewName.Text;
+                            if (pair.Value.Count() > 1)
+                            {
+                                for (int i = 0; i < pair.Value.Count(); i++)
+                                {
+                                    newFileName = txtNewName.Text + "-" + (i + 1);
+                                    try
+                                    {
+                                        File.Move(pair.Key + pair.Value[i], pair.Key + newFileName + Path.GetExtension(pair.Value[i]));
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        txtErrorMessages.AppendText($"Error renaming file at {pair.Key + pair.Value[0]}");
+                                        txtErrorMessages.AppendText(Environment.NewLine);
+                                        txtErrorMessages.AppendText(ex.Message.ToString());
+                                        txtErrorMessages.AppendText(Environment.NewLine);
+                                        txtErrorMessages.AppendText("-----------------------");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    File.Move(pair.Key + pair.Value[0], pair.Key + newFileName + Path.GetExtension(pair.Value[0]));
+                                }
+                                catch (Exception ex)
+                                {
+                                    txtErrorMessages.AppendText($"Error renaming file at {pair.Key + pair.Value[0]}");
+                                    txtErrorMessages.AppendText(Environment.NewLine);
+                                    txtErrorMessages.AppendText(ex.Message.ToString());
+                                    txtErrorMessages.AppendText(Environment.NewLine);
+                                    txtErrorMessages.AppendText("-----------------------");
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("File rename aborted.");
+                    }
+                }
+            }
+            txtFilePaths.Enabled = true;
+            txtFilePaths.Text = "";
+            btnBrowseFiles.Enabled = true;
+            txtNewName.Text = "";
+            txtErrorMessages.AppendText("### File renaming completed! ###");
+        }
+
+        private void ToggleLockFieldsAndButtons(bool isLock)
+        {
+            btnBrowseFiles.Enabled = !isLock;
+            btnUpdate.Enabled = !isLock;
+            txtFilePaths.Enabled = !isLock;
+            txtNewName.Enabled = !isLock;
+        }
+
+        private void txtFilePaths_TextChanged(object sender, EventArgs e)
+        {
+            if (this.Text.Length > 0)
+            {
+                txtNewName.Enabled = true;
+            }
+            else
+            {
+                txtNewName.Enabled = false;
+            }
+        }
+
+        private void txtNewName_TextChanged(object sender, EventArgs e)
+        {
+            if (this.Text.Length > 0)
+            {
+                btnUpdate.Enabled = true;
+            }
+            else
+            {
+                btnUpdate.Enabled = false;
+            }
+        }
+    }
+}
